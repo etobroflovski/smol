2a29609b0ba9f617f5eab6ee26bb7367ee66266d
etobroflovski <70464171+etobroflovski@users.noreply.github.com>
2020-09-05 20:09:52 +0700

    Create default_light.js

---
+++ tumblr/default_light.js
@@ -0,0 +1,8 @@
+var Optica = {};
+Optica.ENDLESS_SCROLLING = false;
+Optica.SHOW_NAV = true;
+Optica.LAYOUT = "grid";
+Optica.GRID_LAYOUT = (Optica.LAYOUT === 'grid');
+Optica.TITLE_COLOR = "#7e7e7e";
+Optica.ACCENT_COLOR = "#263c39";
+Optica.BACKGROUND_COLOR = "#f6f6f6";
