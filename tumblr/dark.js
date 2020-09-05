b7c9df6c4a744865f962ced9231bb0b520250229
etobroflovski <70464171+etobroflovski@users.noreply.github.com>
2020-09-05 20:08:19 +0700

    Create dark.js

---
+++ tumblr/dark.js
@@ -0,0 +1,8 @@
+var Optica = {};
+Optica.ENDLESS_SCROLLING = true;
+Optica.SHOW_NAV = true;
+Optica.LAYOUT = "grid";
+Optica.GRID_LAYOUT = (Optica.LAYOUT === 'grid');
+Optica.TITLE_COLOR = "#949494";
+Optica.ACCENT_COLOR = "#9ad7b9";
+Optica.BACKGROUND_COLOR = "#000000";
